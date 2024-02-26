f556ec9b2ce2d8af6955603e50854a9a556b69a8
vmsalukha <108409602+vmsalukha@users.noreply.github.com>
2024-02-26 23:42:14 +0200

    edit button ArrowCircleLeftOutlinedIcon 3

--- src/components/pokemon/PokemonImg.jsx
+++ src/components/pokemon/PokemonImg.jsx
@@ -1,42 +1,41 @@
-import React, { startTransition } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Paper } from '@mui/material';
 import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import './style.css';
 
-const PokemonImg = ({ pokemon }) => (
+const PokemonImg = ({ pokemon }) => {
+  const navigate = useNavigate();
 
-  <Paper elevation={3} sx={{ width: '30em', height: 'auto', display: 'flex', flexDirection: 'column' }}>
+  const handleClick = () => {
+    navigate('/');
+  };
 
-    <div style={{ alignSelf: 'flex-start' }}>
-    <Link
-        // to="/"
-        style={{ textDecoration: 'none', color: 'inherit' }}
-        onClick={() => {
-          startTransition(() => {
-            window.location.href = "";
-          });
-        }}
-      >
-        <ArrowCircleLeftOutlinedIcon sx={{ fontSize: 40 }} />
-      </Link>
-    </div>
+  return (
+    <Paper elevation={3} sx={{ width: '30em', height: 'auto', display: 'flex', flexDirection: 'column' }}>
+      <div style={{ alignSelf: 'flex-start' }}>
+        <ArrowCircleLeftOutlinedIcon
+          sx={{ fontSize: 40, textDecoration: 'none', color: 'inherit' }}
+          onClick={handleClick}
+        />
+      </div>
 
-    <div style={{ flex: '30%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1 style={{ margin: '0' }}>{pokemon.name}</h1>
-    </div>
+      <div style={{ flex: '30%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <h1 style={{ margin: '0' }}>{pokemon.name}</h1>
+      </div>
 
-    <div style={{ flex: '70%' }}>
-      <img
-        style={{ width: '90%', height: 'auto' }}
-        alt={pokemon.name}
-        src={pokemon.sprites?.other?.home?.front_default || ''}
-      />
-    </div>
-  </Paper>
-);
+      <div style={{ flex: '70%' }}>
+        <img
+          style={{ width: '90%', height: 'auto' }}
+          alt={pokemon.name}
+          src={pokemon.sprites?.other?.home?.front_default || ''}
+        />
+      </div>
+    </Paper>
+  );
+};
 
 PokemonImg.propTypes = {
   pokemon: PropTypes.object.isRequired,
